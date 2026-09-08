83b599b148012902cdcd54b8d51710f9cd5de69e
DekuKotsu <legendaxel29@gmail.com>
2026-09-08 13:16:12 -0600

    Nueva practica

---
+++ Practica2.py
@@ -0,0 +1,13 @@
+""""
+Desarrollar un programa que solicite n cantidad de numeros enteros y almacene en una lista numeros aleatorios entre
+0 y 99. Usando funciones definidas po el usuario
+a) Calcular el elemanto mayor
+b) Calcular el elemento menor
+c) Calcular el promedio de los elementos
+d) Calcular la mediana de los elementos
+e) Calcular la moda de los elementos (incluyendo bimodal, multimodal)
+f) Calcular la desviación estándar poblacional
+"""
+import os
+os.system ("cls")   
+
